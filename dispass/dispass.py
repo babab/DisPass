98d9eaa33bdc0505ca995534c8a47362fe47b832
Benjamin Althues <benjamin@babab.nl>
2012-11-13 23:19:04 +0100

    Define usage/main of dispass in new Dispass class

--- dispass/dispass.py
+++ dispass/dispass.py
@@ -32,172 +32,174 @@ from gui import GUI
 from filehandler import Filehandler
 
 
-def usage():
-    '''Print help / usage information'''
+class Dispass(object):
+    def usage(self):
+        '''Print help / usage information'''
 
-    print "%s - http://dispass.babab.nl/" % (versionStr)
-    print
-    print 'USAGE: dispass [options]'
-    print('       dispass [options] <label> [<label2>] [<label3>] [...]')
-    print '       gdispass'
-    print
-    print 'Options (general):'
-    print '-c, --create    use if this passphrase is new (check input PW)'
-    print '-g, --gui       start guided graphical version of DisPass'
-    print '-h, -?, --help  show this help and exit'
-    print '-o, --output    output passphrases to stdout (instead of the '
-    print '                more secure way of displaying via curses)'
-    print '-V, --version   show full version information and exit'
-    print "--script        optimize input/output for 'wrapping' dispass"
-    print
-    print 'Options (when using labelfile):'
-    print '-s <string>, --search=<string>'
-    print ' ' * 15, 'dispass label from file that uniquely matches <string>'
-    print '-f <labelfile>, --file=<labelfile>'
-    print '                set location of labelfile'
-    print
-    print 'Options (when passing labels as arguments):'
-    print '-l <length>, --length=<length>'
-    print '                set length of passphrase (default: 30, max: 171)'
-    print '-a <algorithm>, --algo=<algorithm>'
-    print '                override algorithm for generating passphrase(s)'
-    print '-n <number>, --number=<number>'
-    print '                override sequence number (default = 1)'
+        print "%s - http://dispass.babab.nl/" % (versionStr)
+        print
+        print 'USAGE: dispass [options]'
+        print('       dispass [options] <label> [<label2>] [<label3>] [...]')
+        print '       gdispass'
+        print
+        print 'Options (general):'
+        print '-c, --create    use if this passphrase is new (check input PW)'
+        print '-g, --gui       start guided graphical version of DisPass'
+        print '-h, -?, --help  show this help and exit'
+        print '-o, --output    output passphrases to stdout (instead of the '
+        print '                more secure way of displaying via curses)'
+        print '-V, --version   show full version information and exit'
+        print "--script        optimize input/output for 'wrapping' dispass"
+        print
+        print 'Options (when using labelfile):'
+        print '-s <string>, --search=<string>'
+        print ' ' * 15, 'dispass label from file that matches <string>'
+        print '-f <labelfile>, --file=<labelfile>'
+        print '                set location of labelfile'
+        print
+        print 'Options (when passing labels as arguments):'
+        print '-l <length>, --length=<length>'
+        print ' ' * 14, 'set length of passphrase (default: 30, max: 171)'
+        print '-a <algorithm>, --algo=<algorithm>'
+        print '                override algorithm for generating passphrase(s)'
+        print '-n <number>, --number=<number>'
+        print '                override sequence number (default = 1)'
 
+    def main(self, argv):
+        '''Entry point and handler of command options and arguments
 
-def main(argv):
-    '''Entry point and handler of command options and arguments
+        :Parameters:
+            - `argv`: List of command arguments
+        '''
 
-    :Parameters:
-        - `argv`: List of command arguments
-    '''
+        execname = argv[0].split('/').pop()
+        console = CLI()
+        a_flag = None
+        f_flag = None
 
-    execname = argv[0].split('/').pop()
-    console = CLI()
-    a_flag = None
-    f_flag = None
+        try:
+            opts, args = getopt.getopt(
+                argv[1:], "a:cf:ghl:n:os:V?",
+                ["algo=", "create", "file=", "gui", "help", "length=",
+                 "number", "output", "script", "search=", "version"])
+        except getopt.GetoptError, err:
+            print str(err), "\n"
+            self.usage()
+            return 2
 
-    try:
-        opts, args = getopt.getopt(argv[1:], "a:cf:ghl:n:os:V?",
-                                   ["algo=", "create", "file=", "gui", "help",
-                                    "length=", "number", "output", "script",
-                                    "search=", "version"])
-    except getopt.GetoptError, err:
-        print str(err), "\n"
-        usage()
-        return 2
+        if args:
+            labels = args
+        else:
+            labels = False
 
-    if args:
-        labels = args
-    else:
-        labels = False
+        for o, a in opts:
+            if o in ("-g", "--gui"):
+                try:
+                    g = GUI()
+                    g.mainloop()
+                except ImportError:
+                    print ('Could not find Tkinter, this is a package needed for'
+                           ' using\n' 'the graphical version of dispass.\n'
+                           'To install, search for a python-tk package for'
+                           ' your OS.\n'
+                           'Arch Linux     \t\t# pacman -S python-tk\n'
+                           'Debian / Ubuntu\t\t$ sudo apt-get install python-tk\n'
+                           'OpenBSD        \t\t# pkg_add -i python-tk')
+                except exceptions.KeyboardInterrupt:
+                    print '\nOk, bye'
+                return
+            elif o in ("-a", "--algo"):
+                algoname = a.lower()
+                if algoname in algos.algorithms:
+                    console.setAlgo(algoname)
+                else:
+                    print 'error: algo "{algo}" does not exist'.format(algo=a)
+                    return 1
+                a_flag = True
+            elif o in ("-n", "--number"):
+                try:
+                    seqno = int(a)
+                except ValueError:
+                    print 'error: sequence number must be a number\n'
+                    self.usage()
+                    return 1
+                console.setSeqNo(seqno)
+            elif o in ("-c", "--create"):
+                console.setPrompt(promptDouble=True)
+            elif o in ("-l", "--length"):
+                try:
+                    length = int(a)
+                except ValueError:
+                    print 'error: length must be a number\n'
+                    self.usage()
+                    return 1
+                console.setLength(length)
+            elif o in ("-f", "--file"):
+                lf = Filehandler(file_location=a)
+                if lf.file_found:
+                    f_flag = a
+                else:
+                    print ('error: could not load labelfile at '
+                           '"{loc}"\n').format(loc=lf.file_location)
+                    return 1
+            elif o in ("-s", "--search"):
+                if f_flag:
+                    lf = Filehandler(file_location=f_flag)
+                else:
+                    lf = Filehandler()
 
-    for o, a in opts:
-        if o in ("-g", "--gui"):
-            try:
-                g = GUI()
-                g.mainloop()
-            except ImportError:
-                print ('Could not find Tkinter, this is a package needed for'
-                       ' using\n' 'the graphical version of dispass.\n'
-                       'To install, search for a python-tk package for'
-                       ' your OS.\n'
-                       'Arch Linux     \t\t# pacman -S python-tk\n'
-                       'Debian / Ubuntu\t\t$ sudo apt-get install python-tk\n'
-                       'OpenBSD        \t\t# pkg_add -i python-tk')
-            except exceptions.KeyboardInterrupt:
-                print '\nOk, bye'
-            return
-        elif o in ("-a", "--algo"):
-            algoname = a.lower()
-            if algoname in algos.algorithms:
-                console.setAlgo(algoname)
+                if lf.file_found:
+                    result = lf.search(a)
+
+                    if not result:
+                        print('{execname}: could not find a label with '
+                              '"{label}" in labelfile'
+                              .format(execname=execname, label=a))
+                        return
+                    elif isinstance(result, int):
+                        print('{execname}: {qty} labels found, please be more '
+                              'specific').format(execname=execname, qty=result)
+                        return
+
+                    console.interactive(result)
+                    return
+                else:
+                    print ('error: could not load labelfile at '
+                           '"{loc}"\n').format(loc=lf.file_location)
+                    return 1
+            elif o in ("-o", "--output"):
+                console.setCurses(False)
+            elif o in ("-h", "-?", "--help"):
+                self.usage()
+                return
+            elif o in ("-V", "--version"):
+                print versionStr, '-', __version_info__, 'running on', os.name
+                return
+            elif o in "--script":
+                console.setScriptableIO()
             else:
-                print 'error: algo "{algo}" does not exist'.format(algo=a)
+                assert False, "unhandled option"
+
+        if labels:
+            console.interactive(labels)
+        else:
+            if a_flag:
+                print('error: option -a can only be used when specifying '
+                      'label(s) as argument(s)')
                 return 1
-            a_flag = True
-        elif o in ("-n", "--number"):
-            try:
-                seqno = int(a)
-            except ValueError:
-                print 'error: sequence number must be a number\n'
-                usage()
-                return 1
-            console.setSeqNo(seqno)
-        elif o in ("-c", "--create"):
-            console.setPrompt(promptDouble=True)
-        elif o in ("-l", "--length"):
-            try:
-                length = int(a)
-            except ValueError:
-                print 'error: length must be a number\n'
-                usage()
-                return 1
-            console.setLength(length)
-        elif o in ("-f", "--file"):
-            lf = Filehandler(file_location=a)
-            if lf.file_found:
-                f_flag = a
-            else:
-                print ('error: could not load labelfile at '
-                       '"{loc}"\n').format(loc=lf.file_location)
-                return 1
-        elif o in ("-s", "--search"):
             if f_flag:
                 lf = Filehandler(file_location=f_flag)
             else:
                 lf = Filehandler()
 
             if lf.file_found:
-                result = lf.search(a)
-
-                if not result:
-                    print ('{execname}: could not find a label with "{label}" '
-                           'in labelfile').format(execname=execname, label=a)
-                    return
-                elif isinstance(result, int):
-                    print ('{execname}: {qty} labels found, please be more '
-                           'specific').format(execname=execname, qty=result)
-                    return
-
-                console.interactive(result)
+                console.interactive(lf.algodict)
                 return
             else:
-                print ('error: could not load labelfile at '
-                       '"{loc}"\n').format(loc=lf.file_location)
+                print('error: could not load labelfile at {loc}\n'
+                      .format(loc=lf.file_location))
+                self.usage()
                 return 1
-        elif o in ("-o", "--output"):
-            console.setCurses(False)
-        elif o in ("-h", "-?", "--help"):
-            usage()
-            return
-        elif o in ("-V", "--version"):
-            print versionStr, '-', __version_info__, 'running on', os.name
-            return
-        elif o in "--script":
-            console.setScriptableIO()
-        else:
-            assert False, "unhandled option"
-
-    if labels:
-        console.interactive(labels)
-    else:
-        if a_flag:
-            print('error: option -a can only be used when specifying label(s)'
-                  ' as argument(s)')
-            return 1
-        if f_flag:
-            lf = Filehandler(file_location=f_flag)
-        else:
-            lf = Filehandler()
-
-        if lf.file_found:
-            console.interactive(lf.algodict)
-            return
-        else:
-            print 'error: could not load labelfile at %s\n' % lf.file_location
-            usage()
-            return 1
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(Dispass().main(sys.argv))
