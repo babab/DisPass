25bc343df3906fafa9fac316ddbee07afe8c32ac
Benjamin Althues <benjamin@babab.nl>
2013-01-29 17:52:16 +0100

    Bump copyright year

--- dispass/filehandler.py
+++ dispass/filehandler.py
@@ -1,6 +1,6 @@
 '''Dispass labelfile handler'''
 
-# Copyright (c) 2011-2012 Benjamin Althues <benjamin@babab.nl>
+# Copyright (c) 2011, 2012, 2013  Benjamin Althues <benjamin@babab.nl>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
--- sphinx-doc/en/conf.py
+++ sphinx-doc/en/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DisPass'
-copyright = u'2011-2012, Benjamin Althues'
+copyright = u'2011, 2012, 2013  Benjamin Althues'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -229,7 +229,7 @@ man_pages = [
 epub_title = u'DisPass'
 epub_author = u'Benjamin Althues'
 epub_publisher = u'Benjamin Althues'
-epub_copyright = u'2011-2012, Benjamin Althues'
+epub_copyright = u'2011, 2012, 2013  Benjamin Althues'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
