137144b65d77691aff5ed2d056da19b15bb2973f
Benjamin Althues <benjamin@babab.nl>
2013-03-24 23:17:43 +0100

    Add 'gui' and 'help' commands

---
+++ dispass/commands/gui.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2011, 2012, 2013  Benjamin Althues <benjamin@babab.nl>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+from ..common import CommandBase
+from ..gui import GUI
+
+
+class Command(CommandBase):
+    usagestr = 'usage: dispass gui'
+    description = 'Start the graphical version of DisPass.'
+    optionList = {'help': ('h', 'show this help information')}
+
+    def run(self):
+        '''Entry point and handler of command options and arguments'''
+        for o, a in self.opts:
+            if o in ("-h", "--help"):
+                print self.usage
+                return
+        try:
+            g = GUI(self.settings)
+            g.mainloop()
+        except ImportError:
+            print ('Could not find Tkinter, this is a package needed '
+                   'for using\n' 'the graphical version of dispass.\n'
+                   'To install, search for a python-tk package for'
+                   ' your OS.\n'
+                   'Arch Linux     \t\t# pacman -S python-tk\n'
+                   'Debian / Ubuntu\t\t$ sudo apt-get install '
+                   'python-tk\n'
+                   'OpenBSD        \t\t# pkg_add -i python-tk')
+            return 2
+        return
---
+++ dispass/commands/help.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2011, 2012, 2013  Benjamin Althues <benjamin@babab.nl>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import exceptions
+import importlib
+
+from ..common import CommandBase
+from ..dispass import Dispass, settings
+
+
+class Command(CommandBase):
+    usagestr = 'usage: dispass help [<command>]'
+    description = 'Show help information'
+
+    def run(self):
+        main = Dispass()
+
+        if not self.args:
+            main.usage()
+            return
+        else:
+            try:
+                mod = importlib.import_module('dispass.commands.'
+                                              + self.args[0])
+                cmd = mod.Command(settings=settings, argv=self.args[1:])
+            except ImportError:
+                print('error: command {cmd} does not exist'
+                      .format(cmd=self.args[0]))
+                return 1
+            except exceptions.KeyboardInterrupt:
+                print('\nOk, bye')
+                return 1
+
+        print(cmd.usage)
