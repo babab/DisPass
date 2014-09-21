062cd691827958faa259821934300c227f12a038
Tom Willemse <tom@ryuslash.org>
2014-09-21 16:20:21 +0200

    Update documentation

--- dispass/dispass.py
+++ dispass/dispass.py
@@ -68,6 +68,8 @@ class DispassCommand(CommandBase):
     description = (
         'Commands:\n'
         '   add          add a new label to labelfile\n'
+        '   disable      disable a label without throwing it away\n'
+        '   enable       enable a label'
         '   generate     generate passphrases for one or more labels\n'
         '   gui          start the graphical version of DisPass\n'
         '   help         show this help information\n'
