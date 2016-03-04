166e09f2bc59ef734e128b233c7d9242fdbf3eed
Benjamin Althues <benjamin@althu.es>
2016-03-04 20:18:33 +0100

    Add and enable sphinx-rtd-theme

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -14,6 +14,9 @@
 import sys
 import os
 
+import sphinx_rtd_theme
+
+
 # load language from doclanguage.py
 sys.path.insert(0, os.path.abspath('.'))
 import doclanguage
@@ -98,7 +101,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -111,7 +114,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
